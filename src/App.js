d3abc52a3a034e725e5941b7387b84d2eda14796
vmagdesyan <vmagdesyan@yandex.ru>
2018-07-07 13:53:38 +0300

    Добавлены компоненты для отображения header, меню и основного контента. Добавлена стили для данных компонентов

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
 import Grid from "react-bootstrap/lib/Grid";
-import Row from "react-bootstrap/lib/Row";
-import Col from "react-bootstrap/lib/Col"
 import styled from "styled-components";
-class App extends Component {
+
+import Header from "./components/Header";
+import Menu from "./components/Menu";
+import Main from "./components/Main";
+
+const url = process.env.PUBLIC_URL + '/img/bg.png';
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 49px;
+  background-color: #242424;
+  opacity: 0.8;
+`
+
+const MenuWrapper = styled.div`
+  height: 89px;
+  padding-top: 21px;
+
+`
+
+const MainWrapper = styled.main`
+  height: 600px;
+  padding-top: 130px;
+  background: url(${url}) no-repeat;
+  backgrount-size: cover;
+`
+class App extends React.Component {
   render() {
     return (
-      <div className="wrapper">
-        <p><h1>Hello, World!</h1></p>
-        <SayFullName name="Vladimir" surname="Magdesyan" link="telegram.org" />
-        <SayFullName name="Pavel" surname="Durov" link="facebook.com" />
+      <div className="App">
+      <HeaderWrapper>
+          <Grid>
+            <Header/>
+          </Grid>
+      </HeaderWrapper> 
+      <MenuWrapper>
+        <Grid>
+          <Menu/>
+        </Grid>
+      </MenuWrapper>
+      <MainWrapper>
+        <Grid>
+          <Main/>
+        </Grid>
+      </MainWrapper>
       </div>
     );
   }
-}
-function SayFullName(props) {
-  return (
-    <div>
-      <h1>Моё имя {props.name}, фамилия {props.surname}</h1>
-      <a href={props.link}>Ссылка на профиль</a>
-    </div>
-  )
 }
 export default App;
